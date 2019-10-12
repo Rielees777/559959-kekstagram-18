8dd22b1d5395830cb63c1cfd07cd5aff7cfa3fa8
Igor <torpos86@mail.ru>
2019-10-12 23:32:50 +0300

    add filters

--- js/gallery.js
+++ js/gallery.js
@@ -9,22 +9,13 @@
 
   var errorTitleElement = errorTemplate.querySelector('.error__title');
 
-  /**
-   * Функция onError обрабатывает ошибки при загрузке данных с сервера
-   * @param {string} message переменная в которую записавается текст сообщения
-   * о вызваной ошибке
-   */
-  var onErrorResponse = function (message) {
-    errorTitleElement.textContent = message;
-    pictureElement.appendChild(errorTemplate);
-  };
 
   /**
     * Функция getPhotosArray отрисовывает на странице  все элементы полученные из массива
     * @param {array} photosArray переменная через которую происходит взаимодействие с каждым элементом
     * массива
     */
-  var getPhotosArray = function (photosArray) {
+  window.getPhotosArray = function (photosArray) {
     for (var i = 0; i < photosArray.length; i++) {
       var newPicture = templatePictures.cloneNode(true);
       newPicture.querySelector('.picture__img').src = photosArray[i].url;
@@ -34,6 +25,23 @@
       pictureElement.appendChild(newPicture);
     }
   };
+  var photos = [];
+  window.updatePhotos = function () {
+    window.getPhotosArray(photos);
+  };
+  /**
+   * Функция onError обрабатывает ошибки при загрузке данных с сервера
+   * @param {string} message переменная в которую записавается текст сообщения
+   * о вызваной ошибке
+   */
+  var onErrorResponse = function (message) {
+    errorTitleElement.textContent = message;
+    pictureElement.appendChild(errorTemplate);
+  };
+  var successHandler = function (data) {
+    photos = data;
+    window.updatePhotos();
+  };
 
-  window.load(getPhotosArray, onErrorResponse, window.utils.URL);
+  window.load(successHandler, onErrorResponse, window.utils.URL);
 })();
