52d6070a77a9017bee8b2c485b1cf9adc161032d
Igor <torpos86@mail.ru>
2019-10-15 10:10:16 +0300

    fix errors in filter code and add comments to the functions

--- js/gallery.js
+++ js/gallery.js
@@ -13,29 +13,43 @@
   window.addEventListener('load', function () {
     imgFilters.classList.remove('img-filters--inactive');
   });
+
   var imageFilterForm = imgFilters.querySelector('.img-filters__form');
+
   var filtersButton = imageFilterForm.querySelectorAll('.img-filters__button');
 
   /**
     * Функция getPhotosArray отрисовывает на странице  все элементы полученные из массива
     * @param {array} photosArray переменная через которую происходит взаимодействие с каждым элементом
     * массива
+    * @param {object} template объект шаблон для вставки картинки
+    * @param {object} parent объект куда вставляются картинки
+    * @param {boolean} clearNode параметр запускающий очистку узла
     */
-  window.getPhotosArray = function (photosArray) {
+  var renderPhotos = function (photosArray, template, parent, clearNode) {
+    if (clearNode) {
+      var children = Array.from(pictureElement.children);
+      children.slice(2, children.length).forEach(function (child) {
+        pictureElement.removeChild(child);
+      });
+    }
+
     for (var i = 0; i < photosArray.length; i++) {
-      var newPicture = templatePictures.cloneNode(true);
+      var newPicture = template.cloneNode(true);
       newPicture.querySelector('.picture__img').src = photosArray[i].url;
       newPicture.querySelector('.picture__likes').textContent = photosArray[i].likes;
       newPicture.querySelector('.picture__comments').textContent = photosArray[i].comments.length;
 
-      pictureElement.appendChild(newPicture);
+      parent.appendChild(newPicture);
     }
   };
-  var photos = [];
-  window.updatePhotos = function () {
-    window.getPhotosArray(photos);
-  };
 
+  var photos = [];
+
+  var succesHandler = function (data) {
+    photos = data;
+    renderPhotos(photos, templatePictures, pictureElement, true);
+  };
   /**
    * Функция onError обрабатывает ошибки при загрузке данных с сервера
    * @param {string} message переменная в которую записавается текст сообщения
@@ -45,13 +59,15 @@
     errorTitleElement.textContent = message;
     pictureElement.appendChild(errorTemplate);
   };
-  var successHandler = function (data) {
-    photos = data;
-    window.updatePhotos();
-  };
 
-  window.load(successHandler, onErrorResponse, window.utils.URL);
+  window.load(succesHandler, onErrorResponse, window.utils.URL);
 
+  /**
+   * Функция обработчик события срабатывающего на форме imageFilterForm
+   * выполняет переключение активного состояние кнопок фильтров и запускает
+   * функции фильтрующие данные с сервера
+   * @param {object} evt объект текущего события
+   */
   imageFilterForm.addEventListener('click', function (evt) {
     evt.preventDefault();
     filtersButton.forEach(function (item) {
@@ -62,7 +78,7 @@
     var id = target.id;
     switch (id) {
       case 'filter-popular':
-        getDefaultPhotos();
+        getPopularPhotos();
         break;
       case 'filter-random':
         getRandomPhotos();
@@ -72,44 +88,53 @@
         break;
     }
   });
-
-  var clearPhotos = function () {
-    var children = Array.from(pictureElement.children);
-    children.slice(2, children.length).forEach(function (child) {
-      pictureElement.removeChild(child);
-    });
+  /**
+   * Функция getPopularPhotos делает копию загруженного массива с фото
+   * после чего рендерит их через функцию renderPhotos
+   */
+  var getPopularPhotos = function () {
+    var popularPhotos = photos.slice();
+    renderPhotos(popularPhotos, templatePictures, pictureElement, true);
   };
 
-  var getDefaultPhotos = function () {
-    photos.slice();
-    clearPhotos();
-    window.updatePhotos();
-  };
-
-  var getRandomPhoto = function () {
-    var randomIndex = window.utils.getRandomNum(0, photos.length);
+  /**
+   * Функция getRandomPic назначает случайный индекс в переменную randomIndex
+   * и через него возвращает случайный объект из массива данных с сервера
+   * @return {object} photos[randomIndex] случайный объект из массива данных с сервера
+   */
+  var getRandomPic = function () {
+    var randomIndex = window.utils.getRandomNum(0, photos.length - 1);
     return photos[randomIndex];
   };
 
+  /**
+   * Функция getRandomPhotos выводит 10 случайных фото из данных с сервера
+   * создаем пустой массив randomPhotos, затем с помощью итератора перебираем
+   * массив объектов с сервера затем берем случайный элемент и проверяем есть этот элемент в
+   * массиве, а также колличество добавленых элементов, после добавляем элемент
+   * в массив randomPhotos и далее рендерит его через renderPhotos
+   */
   var getRandomPhotos = function () {
-    photos.filter(function (photo) {
-      var photosList = [];
-      for (var i = 0; i < 10; i++) {
-        photo = getRandomPhoto();
-        if (!photosList.includes(photo)) {
-          photosList.push(photo);
-        }
+    var randomPhotos = [];
+    photos.forEach(function (randImg) {
+      randImg = getRandomPic();
+      if (!randomPhotos.includes(randImg) && randomPhotos.length < 10) {
+        randomPhotos.push(randImg);
       }
     });
-    clearPhotos();
-    window.updatePhotos();
+    renderPhotos(randomPhotos, templatePictures, pictureElement, true);
   };
 
+  /**
+   * Функция getCommentPhotos сортирует массив данных ссервера по
+   * количеству комментариев начиная с наибольшего и рендерит их
+   * через функцию renderPhotos
+   */
   var getCommentPhotos = function () {
-    photos.sort(function (current, next) {
-      return current.commentCount - next.commentCount;
+    var commentedPhotos = photos.slice();
+    commentedPhotos.sort(function (current, next) {
+      return next.comments.length - current.comments.length;
     });
-    clearPhotos();
-    window.updatePhotos();
+    renderPhotos(commentedPhotos, templatePictures, pictureElement, true);
   };
 })();
